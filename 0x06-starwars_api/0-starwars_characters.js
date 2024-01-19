6996ac68fbf0d7c6ee8219f98860e68b74944aea
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-01-18 18:14:03 -0800

    done with starwars Api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,39 @@
 #!/usr/bin/node
- 
-const args = process.argv.slice(2)
-const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/1/', (error, response, body) => {
-  if (error) {
-    // Handle errors
-    console.error('Error:', error);
-  } else {
-    // Handle the response body
-    console.log('Response:', body);
-  }
-});
+const args = process.argv.slice(2);
+const part = args[0];
+const request = require('request-promise-native');
+
+function getData (url) {
+  return request(url)
+    .then(body => {
+      return JSON.parse(body);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      throw error;
+    });
+}
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + part + '/';
+getData(url)
+  .then(data => {
+    const charList = data.characters;
+    const charPromises = charList.map(charUrl =>
+      getData(charUrl)
+        .then(charData => {
+          return charData.name;
+        })
+        .catch(charError => {
+          console.error('Error getting character data:', charError);
+        })
+    );
+    return Promise.all(charPromises);
+  })
+  .then(result => {
+    for (const name of result) {
+      console.log(name);
+    }
+  })
+  .catch(error => {
+    console.error('Error getting data:', error);
+  });
